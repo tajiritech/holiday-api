7774bb1122be272d3657323d8b7fd227a12130e4
kengo <tajiri.teckbiz@gmail.com>
2026-03-17 14:53:06 +0900

    first commit

--- fetch/get_holidays.php
+++ fetch/get_holidays.php
@@ -3,5 +3,5 @@
 $url = "https://holidays-jp.github.io/api/v1/date.json";
 $json = file_get_contents($url);
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-$dir = './json/holiday.json';
+$dir = '../json/holiday.json';
 file_put_contents($dir, $json, LOCK_EX);
